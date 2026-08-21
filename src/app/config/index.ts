0de737dc75f0ffe045eebaa8abe5f3ce096505f7
raghuraman965 <raghuraman965@gmail.com>
2026-08-21 09:10:57 +0530

    Fix production API URL

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,2 +1,2 @@
-export const url: string = 'http://localhost:5000/';
+export const url: string = 'https://tedbus-958t.onrender.com/';
 export const googleMapsApiKey: string = 'AIzaSyD137fp6DkDJOKYXic2tZKTvjnILdHQr_U';
